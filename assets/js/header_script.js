e9ac042a1475ec688f79f8e644c5c01f9e8179f9
Quinn <quinnsmyers@gmail.com>
2016-08-10 16:05:28 -0700

    fixed widths and x-overflow

--- assets/js/header_script.js
+++ assets/js/header_script.js
@@ -1,10 +1,10 @@
-//need to try and add some slide down functionality later
-//slideToggle does work sorta but it's very bugyy
+
 $(document).ready(function () {
   $("#mobile-button").on('click', function() {
     $(".mobile-nav").toggleClass("is-open");
   });
 });
+
 
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
@@ -20,6 +20,7 @@ $(function() {
     }
   });
 });
+
 
 
 
